aff27814eeb6f0bb78242536480246baf544a90f
DL <DL@163.com>
2021-09-23 14:37:04 +0800

    feat(local):修复;

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,7 +11,7 @@
 <script setup>
 </script>
 
-<style scoped lang="stylus">
+<style scoped >
 .home-container {
   top: 50%;
   display: grid;
@@ -22,25 +22,25 @@
   position: absolute;
   transform: translateY(-50%);
   grid-template-columns: repeat(5, 1fr);
+}
 
-  .box-juce {
-    background: #ccc;
-  }
+.box-juce {
+  background: #ccc;
+}
 
-  .box-aimi {
-    background: #eee;
-  }
+.box-aimi {
+  background: #eee;
+}
 
-  .box-kaiqi {
-    background: #c2c2c2;
-  }
+.box-kaiqi {
+  background: #c2c2c2;
+}
 
-  .box-bruce {
-    background-color: #d3c3c3;
-  }
+.box-bruce {
+  background-color: #d3c3c3;
+}
 
-  .box-aimili {
-    background-color: #4a4a4a;
-  }
+.box-aimili {
+  background-color: #4a4a4a;
 }
 </style>
