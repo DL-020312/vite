eac8ac969b954f6c4d294a657ff4b9d70127989d
DL <DL@163.com>
2021-09-23 14:33:46 +0800

    feat(local):init;

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Home from "./components/Home.vue";
+</script>
+
+<template>
+  <Home msg.prop="123"></Home>
+</template>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home-container">
+    <div class="box-juce"></div>
+    <div class="box-aimi"></div>
+    <div class="box-kaiqi"></div>
+    <div class="box-bruce"></div>
+    <div class="box-aimili"></div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped lang="stylus">
+.home-container {
+  top: 50%;
+  display: grid;
+  height: 400px;
+  width: 100vw;
+  background: #ddd;
+  place-self: center;
+  position: absolute;
+  transform: translateY(-50%);
+  grid-template-columns: repeat(5, 1fr);
+
+  .box-juce {
+    background: #ccc;
+  }
+
+  .box-aimi {
+    background: #eee;
+  }
+
+  .box-kaiqi {
+    background: #c2c2c2;
+  }
+
+  .box-bruce {
+    background-color: #d3c3c3;
+  }
+
+  .box-aimili {
+    background-color: #4a4a4a;
+  }
+}
+</style>
